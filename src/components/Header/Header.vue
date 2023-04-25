e07914d3184af2597935eb7d48ea40ad6157774d
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-04-25 11:32:33 -0400

    feat: ✨ add components Header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<header class="relative">
+		<HeaderControls @hamburger-clicked="toggleNav" />
+		<HeaderNav />
+	</header>
+</template>
+
+<script setup>
+const isNavVisible = ref(false)
+provide('isNavVisible', isNavVisible)
+const toggleNav = () => {
+	isNavVisible.value = !isNavVisible.value
+}
+</script>
